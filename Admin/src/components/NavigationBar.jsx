cf08ac78a7ba30644d97cc58a08d0db0bfa60563
Ayush Choudhary <ayushdedha23@gmail.com>
2025-07-29 01:59:46 +0530

    NavigationBar and Login done

--- Admin/src/components/NavigationBar.jsx
+++ Admin/src/components/NavigationBar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { NavLink, useNavigate} from 'react-router-dom'
 
 const NavigationBar = () => {
+  let navigate = useNavigate();
+
+  let handleLogout = ()=>{
+    sessionStorage.clear();
+    navigate('/login');
+  }
+
   return (
-    <div>NavigationBar</div>
+    <div className='h-screen w-[260px] flex flex-col gap-15 items-center border-r-2 bg-black text-white'>
+      <h2 className='mt-8 font-bold text-2xl'>Real-Estate</h2>
+      <ul className='grow flex flex-col '>
+        <NavLink to={'/projects'}><li className='border-l-2 border-t-2 border-r-2 border-gray-500 w-[250px] p-4 text-white'>Projects</li></NavLink>
+        <NavLink to={'/properties'}><li className='border-2 border-gray-500 w-[250px] p-4 text-white'>Properties</li></NavLink>
+        <NavLink to={'/enquiries'}><li className='border-2 border-t-0 border-gray-500 w-[250px] p-4 text-white'>Enquiries</li></NavLink>
+      </ul>
+      <p onClick={handleLogout} className='flex items-cente font-semibold cursor-pointer transition duration-350 ease-in-out transform hover:scale-115 hover:text-red-500 mb-3'>Logout</p>
+    </div>
   )
 }
 
--- Admin/src/pages/authentication/Login.jsx
+++ Admin/src/pages/authentication/Login.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import API from '../../util/Api.js'
+import {toast} from 'react-toastify'
 
 const Login = () => {
+  let [email, setEmail] = useState('');
+  let [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e)=>{
+    e.preventDefault(e);
+    
+    try {
+      let response = await API.post('/admin/login', {
+        email:email,
+        password_hash:password
+      });
+
+      let token = response.data.token;
+      sessionStorage.setItem('AuthToken', token);
+      sessionStorage.setItem('isAuthenticated', true);
+      
+      toast.success(`Successfully Logged In`);
+      navigate('/projects');
+    } catch (error) {
+      console.log(error);
+      toast.error(`Login Failed!`);
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen flex justify-center items-center bg-gray-50'>
+      <form className = "flex flex-col h-[21rem] w-[22rem] border-2 rounded-[10px] p-6 items-center shadow-md bg-white" onSubmit={handleLogin}
+      >
+        <h1 className="mb-4 !text-2xl font-semibold">Login</h1>
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" placeholder="Enter your email" className="border p-2 rounded mb-2" value={email} onChange={(e)=>setEmail(e.target.value)} required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder="Enter your password" className="border p-2 rounded outline-none" value={password} onChange={(e)=>setPassword(e.target.value)}
+            />
+          </div>
+          <button className = "mt-6 bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Login</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
