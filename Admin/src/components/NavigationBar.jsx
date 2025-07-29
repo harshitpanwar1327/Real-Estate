a002f3dfee83cc5de3130676ca39355f73cc2214
harshitpanwar1327 <harshitpanwar1327@gmail.com>
2025-07-29 22:20:44 +0530

    login checked and navigation bar updated

--- Admin/src/components/NavigationBar.jsx
+++ Admin/src/components/NavigationBar.jsx
@@ -9,14 +9,14 @@ const NavigationBar = () => {
   }
 
   return (
-    <div className='h-screen w-[260px] flex flex-col gap-15 items-center border-r-2 bg-black text-white'>
-      <h2 className='mt-8 font-bold text-2xl'>Real-Estate</h2>
-      <ul className='grow flex flex-col '>
-        <NavLink to={'/projects'}><li className='border-l-2 border-t-2 border-r-2 border-gray-500 w-[250px] p-4 text-white'>Projects</li></NavLink>
-        <NavLink to={'/properties'}><li className='border-2 border-gray-500 w-[250px] p-4 text-white'>Properties</li></NavLink>
-        <NavLink to={'/enquiries'}><li className='border-2 border-t-0 border-gray-500 w-[250px] p-4 text-white'>Enquiries</li></NavLink>
-      </ul>
-      <p onClick={handleLogout} className='flex items-cente font-semibold cursor-pointer transition duration-350 ease-in-out transform hover:scale-115 hover:text-red-500 mb-3'>Logout</p>
+    <div className='h-screen w-[220px] flex flex-col items-center gap-10 bg-black text-white'>
+      <h2 className='mt-8 font-semibold text-2xl'>Real Estate</h2>
+      <div className='grow w-full'>
+        <NavLink to={'/projects'}><div className='border-b border-gray-500 p-4 text-white'>Projects</div></NavLink>
+        <NavLink to={'/properties'}><div className='border-b border-gray-500 p-4 text-white'>Properties</div></NavLink>
+        <NavLink to={'/enquiries'}><div className='border-b border-gray-500 p-4 text-white'>Enquiries</div></NavLink>
+      </div>
+      <button onClick={handleLogout} className='mb-4 hover:!text-red-500'>Logout</button>
     </div>
   )
 }
--- Admin/src/pages/authentication/Login.jsx
+++ Admin/src/pages/authentication/Login.jsx
@@ -14,40 +14,36 @@ const Login = () => {
     
     try {
       let response = await API.post('/admin/login', {
-        email:email,
-        password_hash:password
+        email: email,
+        password_hash: password
       });
 
       let token = response.data.token;
       sessionStorage.setItem('AuthToken', token);
       sessionStorage.setItem('isAuthenticated', true);
       
-      toast.success(`Successfully Logged In`);
+      toast.success(`Login Successfull`);
       navigate('/projects');
     } catch (error) {
       console.log(error);
-      toast.error(`Login Failed!`);
+      toast.error(response?.error?.data?.message || `Login Failed!`);
     }
   }
 
   return (
-    <div className='w-screen h-screen flex justify-center items-center bg-gray-50'>
-      <form className = "flex flex-col h-[21rem] w-[22rem] border-2 rounded-[10px] p-6 items-center shadow-md bg-white" onSubmit={handleLogin}
+    <div className='grow flex justify-center items-center'>
+      <form className = "flex flex-col items-center w-1/4 border border-[#cdcdcd] rounded-[10px] p-5 shadow-lg gap-2" onSubmit={handleLogin}
       >
-        <h1 className="mb-4 !text-2xl font-semibold">Login</h1>
+        <h2 className="mb-2 text-2xl font-semibold">Login</h2>
         <div className="flex flex-col w-full">
-          <div className="flex flex-col">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="Enter your email" className="border p-2 rounded mb-2" value={email} onChange={(e)=>setEmail(e.target.value)} required
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="Enter your password" className="border p-2 rounded outline-none" value={password} onChange={(e)=>setPassword(e.target.value)}
-            />
-          </div>
-          <button className = "mt-6 bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Login</button>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" className="border p-2 rounded" value={email} onChange={(e)=>setEmail(e.target.value)} required />
         </div>
+        <div className="flex flex-col w-full">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" className="border p-2 rounded" value={password} onChange={(e)=>setPassword(e.target.value)} />
+        </div>
+        <button className = "w-1/2 mt-4 bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Login</button>
       </form>
     </div>
   )
