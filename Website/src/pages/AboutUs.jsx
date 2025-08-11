e26d56e1ef13cddb288e5bfc082e44d471609919
Ayush Choudhary <ayushdedha23@gmail.com>
2025-08-11 19:02:10 +0530

    About Us page done

--- Website/src/pages/AboutUs.jsx
+++ Website/src/pages/AboutUs.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+
 
 const AboutUs = () => {
+
   return (
-    <div>AboutUs</div>
+    <>
+      <div className="flex items-center justify-center h-[50vh] lg:h-[70vh] bg-[url(/src/assets/about-us.webp)] bg-cover bg-center">
+      </div>
+      <section className="px-6 md:px-16 py-16 bg-white">
+        <div className="max-w-8xl mx-auto">
+          <h2 className="text-2xl md:text-3xl font-bold text-black">
+            CREATING BETTER <br />
+            <span className="text-yellow-600 pt-10">COMMUNITIES</span>
+          </h2>
+          <p className="mt-4 text-gray-700">
+            Property Shell has been one of the most premium real estate developers in India with more than 5 years of experience in this business. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi assumenda nostrum ex. Quam neque sit hic nemo magnam excepturi dolorum eligendi impedit aut, porro cumque voluptas provident culpa ab nobis recusandae, commodi maxime sed suscipit possimus. Dignissimos voluptatum eaque, exercitationem numquam ratione eveniet quidem pariatur minus asperiores enim aut laborum cum architecto deserunt debitis sed voluptas voluptatibus, molestias possimus beatae error? Cupiditate consequuntur repellat at nulla error vitae nam saepe, dolorem ad in excepturi, itaque, laborum quae quo suscipit fugiat corrupti explicabo ut eius doloremque velit dicta! Odit iste voluptatum, quos nisi sunt veritatis. Recusandae animi aliquid nulla esse doloribus.
+          </p>
+        </div>
+
+        <div className="max-w-10xl mt-12 grid md:grid-cols-2 gap-8 items-center space-y-12">
+          <img
+            src="/src/assets/HighriseBuilding.jpg"
+            alt="Property Shell Project"
+            className="w-full rounded h-[50vh] shadow-md"
+          />
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold text-yellow-600">
+              OUR MISSION
+            </h3>
+            <p className="mt-4 text-gray-700">
+              At <span className="text-red-500 font-semibold">Property Shell</span>, Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi quam incidunt aliquid nemo ea voluptates illum beatae, autem recusandae dolore delectus! Doloremque, ipsa dicta. Voluptatibus, sequi ad a itaque natus sunt magnam culpa optio non id est minima nesciunt accusamus dolores. Laudantium debitis repellat aperiam corrupti voluptatem quia ut odit neque consequuntur earum velit amet voluptates provident, iste voluptatum, rerum soluta omnis ex dolore eius ab quis odio eaque magni. Saepe voluptates, sit, enim provident facilis voluptas quod veritatis neque iure pariatur nisi, architecto a placeat ullam odit non unde deleniti adipisci aliquid dolores ad. Dolorum sunt quas expedita! Aliquam deserunt ab cumque, dignissimos nisi sapiente, itaque dolores, quisquam similique provident reprehenderit? Veritatis dolorem obcaecati, eum tempora veniam saepe hic tenetur, praesentium at enim quisquam tempore doloribus error libero, consectetur 
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-2xl md:text-3xl font-bold text-yellow-600">
+              OUR VISION
+            </h3>
+            <p className="mt-4 text-gray-700">
+              At Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi quam incidunt aliquid nemo ea voluptates illum beatae, autem recusandae dolore delectus! Doloremque, ipsa dicta. Voluptatibus, sequi ad a itaque natus sunt magnam culpa optio non id est minima nesciunt accusamus dolores. Laudantium debitis repellat aperiam corrupti voluptatem quia ut odit neque consequuntur earum velit amet voluptates provident, iste voluptatum, rerum soluta omnis ex dolore eius ab quis odio eaque magni. Saepe voluptates, sit, enim provident facilis voluptas quod veritatis neque iure pariatur nisi, architecto a placeat ullam odit non unde deleniti adipisci aliquid dolores ad. Dolorum sunt quas expedita! Aliquam deserunt ab cumque, dignissimos nisi sapiente, itaque dolores, quisquam similique provident reprehenderit? Veritatis dolorem obcaecati, eum tempora veniam saepe hic tenetur, praesentium at enim quisquam tempore doloribus error libero, consectetur 
+            </p>
+          </div>
+          <img
+            src="/src/assets/TechOpenSpace.jpg"
+            alt="Property Shell Project"
+            className="w-full rounded h-[50vh] shadow-md"
+          />
+        </div>
+      </section>
+
+      <section className="py-12 px-4 md:px-8 lg:px-16 bg-white text-center">
+        <h2 className="text-2xl md:text-3xl font-bold mb-4">
+          INNOVATING SPACES{" "}
+          <span className="text-yellow-500">FOR A BETTER FUTURE</span>
+        </h2>
+
+        <p className="max-w-7xl mx-auto text-gray-600 mb-10">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur quas cupiditate molestiae nemo nam blanditiis! Aliquam dolor facere obcaecati, voluptatibus consequuntur sed eveniet animi voluptates fuga. Enim delectus ex alias libero est cum quaerat placeat nulla nesciunt expedita neque cumque voluptatum, facilis vitae. Eum possimus sequi deleniti commodi dolorem dolor.
+        </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="flex items-end justify-center text-white h-[50vh] lg:h-[50vh] text-base lg:text-lg font-bold bg-[url(/src/assets/prime.png)] bg-cover bg-center rounded hover:text-3xl hover:items-center">
+            <h2 className='p-2 rounded-lg'>PRIME LOCATION</h2>
+          </div>
+          <div className="flex items-end justify-center text-white h-[50vh] lg:h-[50vh] text-base lg:text-lg font-bold bg-[url(/src/assets/global.png)] bg-cover bg-center rounded hover:text-3xl hover:items-center">
+            <h2 className='p-2 rounded-lg'>GLOBAL BUSINESS HUB</h2>
+          </div>
+          <div className="flex items-end justify-center text-white h-[50vh] lg:h-[50vh] text-base lg:text-lg font-bold bg-[url(/src/assets/comprehensive.png)] bg-cover bg-center rounded hover:text-3xl hover:items-center">
+            <h2 className='p-2'>CONPREHENSIVE INFRASTRUCTURE</h2>
+          </div>
+          <div className="flex items-end justify-center text-white h-[50vh] lg:h-[50vh] text-base lg:text-lg font-bold bg-[url(/src/assets/sustainability.png)] bg-cover bg-center rounded hover:text-3xl hover:items-center">
+            <h2 className='p-2 rounded-lg'>SUSTAINABILITY AND INNOVATION</h2>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
